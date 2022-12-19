7a7517edd5e7cf79f6a1457b58e4a98dde2807ff
kpxcrew <118790881+kpxcrew@users.noreply.github.com>
2022-12-19 08:22:36 -0600

    number character criteria added and readme updated

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var userChoice =prompt("how many");
+  if (userChoice >= 8 && userChoice < 128) {//continue 
+    //another fuction for next criteria
+} else {alert ("Must be within 8 and 128") 
+    generatePassword()
+  }
+ 
+  
+}
+ function specialCriteria(){
+  var userChoiceSpecial =confirm ("Are you sure");
+
+ }

6ba140a6e135d6f3eb2282226035c37f41fc4309
kpxcrew <118790881+kpxcrew@users.noreply.github.com>
2022-12-20 02:46:57 -0600

    code generating based on criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
-var characterLength = 8;
-var choiceArr = [];
+var characterLength = 0;
+var choiceArray = [];
 
 // Array of second criteria
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -15,10 +15,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var yesPrompts = getCriteria();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    if (yesPrompts) {
+        var newPassword = generatePassword();
+        passwordText.value = newPassword;
+    } else {
+        passwordText.value = "";
+    }
 
 }
 
@@ -27,20 +32,39 @@ generateBtn.addEventListener("click", writePassword);
 
 // new function for generate pass
 function generatePassword(){
- 
-  var userChoice =prompt("How many characters do you want your password to be?");
-  if (userChoice >= 8 && userChoice <= 128) {//continue 
-    //another fuction for next criteria
-} else {alert ("Must be within 8 and 128") 
-    generatePassword()
+    var password = "";
+    for (var index = 0; index < characterLength; index++) {
+        var randomNumber = Math.floor(Math.random() * choiceArray.length);
+        password = password + choiceArray[randomNumber];
+        }
+        return password;
+}
+//function for
+function getCriteria(){
+  choiceArray = [];
+      //parseInt make into a number
+    characterLength = parseInt(prompt("How many characters do you want your password to be?"));
+    
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert ("Must be a number within 8 and 128") 
+      return getCriteria();
+      
   }
 
+  if (confirm("Include lowercase characters?")) {
+    choiceArray = choiceArray.concat(lowerCaseArray);
+  }
 
+  if (confirm("Include uppercase characters?")) {
+    choiceArray = choiceArray.concat(upperCaseArray);
+  }
 
-  return "generated password"
-  
+  if (confirm("Include number characters?")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+
+  if (confirm("Include special characters?")) {
+    choiceArray = choiceArray.concat(specialCharacterArray);
+  }
+  return true;
 }
- function specialCriteria(){
-  var userChoiceSpecial =confirm ("Are you sure");
-
- }
